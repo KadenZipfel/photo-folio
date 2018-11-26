4436dbad846b4c95894c4e890b8ca8ab25154e21
unknown <kaden.zipfel@hotmail.com>
2018-11-25 16:23:30 -0800

    Essentially complete

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
